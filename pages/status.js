b369a46c71d6448120a75cd8110df47f64e348f9
CalieR <calielrushton@gmail.com>
2019-06-08 18:48:43 +0100

    start heroku setup

--- pages/status.js
+++ pages/status.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 
 const headers = { "Content-Type": "application/json" };
-
+const API_BASE_URL = "https://powerful-stream-53019.herokuapp.com";
+console.log(API_BASE_URL);
 const Status = props => (
   <>
     <h1>{props.result}</h1>
@@ -10,19 +11,19 @@ const Status = props => (
 );
 
 const getHealthCheck = async () => {
-  let res = await fetch("http://localhost:3002/api/health/ping", {
+  let healthUrl = `${API_BASE_URL}/api/health/ping`;
+  let res = await fetch(healthUrl, {
     headers
-  })
-  let data = await res.json()
+  });
+  let data = await res.json();
 
   return {
     result: data.result
-  }
-
+  };
 };
 
 Status.getInitialProps = () => {
-  return getHealthCheck()
+  return getHealthCheck();
 };
 
 export default Status;
